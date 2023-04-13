9599230b79a7b6a1ca1a6a29ec7789464b1ecb1b
Tonnyb <tonnyb@joingladys.com>
2023-04-13 17:20:28 +0300

    initializes app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App

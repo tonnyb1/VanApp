3f0916d6d674ca8b4a95278dc801e88e71dd8bd7
Tonnyb <tonnyb@joingladys.com>
2023-04-14 11:45:16 +0300

    adds vans page and individual van page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
+import Home from './pages/Home'
+import About from './pages/About'
+import Vans from './pages/Vans'
+import VanDetail from './pages/VanDetail'
 
+import './server'
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-        </nav>
-      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:vanId" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )

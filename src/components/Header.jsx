fb649725960a0ebaa37e427b5b01d0a5d332da9a
Tonnyb <tonnyb@joingladys.com>
2023-04-16 19:27:26 +0300

    adds vans and host page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
     return (
         <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
+            <Link className="site-logo" to="/">#VanLife</Link>
+            <nav>
+            <Link to="/host">Host</Link>
+            <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
+            </nav>
+        </header>
     )
 }
--- src/pages/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-    return (
-        <footer className="footer">
-            <p className="foot-para">Ⓒ 2022 #VANLIFE</p>
-        </footer>
-    );
-}

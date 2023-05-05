5327fb692d7826d32e08ff030851a654d2f547ab
Tonnyb <tonnyb@joingladys.com>
2023-05-05 21:19:17 +0300

    adds navigation on protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,25 @@ import {
   Route,
   Link
 } from "react-router-dom"
-import './App.css'
+import "./App.css"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans, { loader as vansLoader } from "./pages/Vans/Vans"
-import VanDetail from "./pages/Vans/VanDetail"
+import VanDetail, { loader as vanDetailLoader } from "./pages/Vans/VanDetail"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
-import HostVans from "./pages/Host/HostVans"
-import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVans, { loader as hostVansLoader} from "./pages/Host/HostVans"
+import HostVanDetail, { loader as hostVanDetailLoader } from "./pages/Host/HostVanDetail"
 import HostVanInfo from "./pages/Host/HostVanInfo"
 import HostVanPricing from "./pages/Host/HostVanPricing"
 import HostVanPhotos from "./pages/Host/HostVanPhotos"
 import NotFound from "./pages/NotFound"
-import Login from "./pages/Login"
+import Login, { loader as loginLoader, action as loginAction } from "./pages/Login"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
 import Error from "./components/Error"
+import { requireAuth } from "./utils"
 
 import "./server"
 
@@ -35,6 +36,8 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route
       path="login"
       element={<Login />}
+      loader={loginLoader}
+      action={loginAction}
     />
     <Route
       path="vans"
@@ -42,17 +45,53 @@ const router = createBrowserRouter(createRoutesFromElements(
       errorElement={<Error />}
       loader={vansLoader}
     />
-    <Route path="vans/:id" element={<VanDetail />} />
+    <Route 
+      path="vans/:id" 
+      element={<VanDetail />} 
+      loader={vanDetailLoader}
+    />
 
     <Route path="host" element={<HostLayout />}>
-      <Route index element={<Dashboard />} />
-      <Route path="income" element={<Income />} />
-      <Route path="reviews" element={<Reviews />} />
-      <Route path="vans" element={<HostVans />} />
-      <Route path="vans/:id" element={<HostVanDetail />}>
-        <Route index element={<HostVanInfo />} />
-        <Route path="pricing" element={<HostVanPricing />} />
-        <Route path="photos" element={<HostVanPhotos />} />
+      <Route
+        index
+        element={<Dashboard />}
+        loader={async ({ request }) => await requireAuth(request)}
+      />
+      <Route
+        path="income"
+        element={<Income />}
+        loader={async ({ request }) => await requireAuth(request)}
+      />
+      <Route
+        path="reviews"
+        element={<Reviews />}
+        loader={async ({ request }) => await requireAuth(request)}
+      />
+      <Route
+        path="vans"
+        element={<HostVans />}
+        loader={hostVansLoader}
+      />
+      <Route
+        path="vans/:id"
+        element={<HostVanDetail />}
+        loader={hostVanDetailLoader}
+      >
+        <Route
+          index
+          element={<HostVanInfo />}
+          loader={async ({ request }) => await requireAuth(request)}
+        />
+        <Route
+          path="pricing"
+          element={<HostVanPricing />}
+          loader={async ({ request }) => await requireAuth(request)}
+        />
+        <Route
+          path="photos"
+          element={<HostVanPhotos />}
+          loader={async ({ request }) => await requireAuth(request)}
+        />
       </Route>
     </Route>
     <Route path="*" element={<NotFound />} />
